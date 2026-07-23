1ce48400e5a8187fd3a36a16fe5c4405240e4efa
SHAH <SHAHKHN>
2026-07-23 02:58:08 +0100

    modidfied index.js and test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+console.log('hello world')
+
+setTimeout(() => {
+  console.log('This message is delayed by 2 seconds');
+}, 3000);
+
+console.log('This message is logged immediately after the first one');
